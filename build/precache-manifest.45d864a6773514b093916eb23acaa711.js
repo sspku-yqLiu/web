66b9f8318c78116c1e0ac03c3d4ef6fbf799c3fa
sspku-yqLiu <liuyongqi@pku.edu.cn>
2020-08-18 08:39:50 +0800

    pusblish

---
+++ build/precache-manifest.45d864a6773514b093916eb23acaa711.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ca2c44f8f8596267f7d0504eb0031be",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "e166071e37cb2aec4447",
+    "url": "/web/static/css/2.63b65d4e.chunk.css"
+  },
+  {
+    "revision": "a6a9bfdd04e33e0b4218",
+    "url": "/web/static/css/main.98a568d3.chunk.css"
+  },
+  {
+    "revision": "e166071e37cb2aec4447",
+    "url": "/web/static/js/2.feb57421.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/web/static/js/2.feb57421.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6a9bfdd04e33e0b4218",
+    "url": "/web/static/js/main.a402b701.chunk.js"
+  },
+  {
+    "revision": "be173b3bc9ad4af99df0",
+    "url": "/web/static/js/runtime-main.09206de2.js"
+  },
+  {
+    "revision": "d219237d5a1ab1e6869f43dd2c50647b",
+    "url": "/web/static/media/BILIBILI_LOGO.d219237d.svg"
+  },
+  {
+    "revision": "34ead385b2b16466efffc889c884a141",
+    "url": "/web/static/media/GitHub.34ead385.svg"
+  },
+  {
+    "revision": "ecd231951e9bfbfffa2f871a9a262cd1",
+    "url": "/web/static/media/ICE.ecd23195.png"
+  },
+  {
+    "revision": "d35e4e92b13e5d17788a1c4ea61eda3b",
+    "url": "/web/static/media/LOGO.d35e4e92.svg"
+  },
+  {
+    "revision": "9a1c070c66f7c0f2c09b1dfb486778dc",
+    "url": "/web/static/media/PKU.9a1c070c.png"
+  },
+  {
+    "revision": "1bca28a27df798afefa05573d64ee5b5",
+    "url": "/web/static/media/backgroud.1bca28a2.jpg"
+  },
+  {
+    "revision": "a392ea8687a157890917e29c66fc92bc",
+    "url": "/web/static/media/demo01.a392ea86.png"
+  },
+  {
+    "revision": "0d9cf1045ff7e6f8fa65f2871e2f42f3",
+    "url": "/web/static/media/demo1.0d9cf104.png"
+  },
+  {
+    "revision": "d2d24c6cf7beb14e5411d135edd0c281",
+    "url": "/web/static/media/demo2.d2d24c6c.png"
+  },
+  {
+    "revision": "cae61369e8398792856d1f20c6025b33",
+    "url": "/web/static/media/demo3.cae61369.png"
+  },
+  {
+    "revision": "9158fb2badf0837c920ea25a27a57dd7",
+    "url": "/web/static/media/devui.9158fb2b.svg"
+  },
+  {
+    "revision": "70ca16b8d8e133d675e4665a07b5e790",
+    "url": "/web/static/media/huawei.70ca16b8.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ea03387d9480fd33e7ae5192eeb70b12",
+    "url": "/web/static/media/materialCenterDemo.ea03387d.png"
+  },
+  {
+    "revision": "e71a6e51367943846456376ee764dfad",
+    "url": "/web/static/media/me.e71a6e51.jpg"
+  },
+  {
+    "revision": "9230d16e12d326cc1dcdc16ab0eb1888",
+    "url": "/web/static/media/学位.9230d16e.svg"
+  },
+  {
+    "revision": "4a1c08b43443bb407e2088e1c7e64139",
+    "url": "/web/static/media/微信.4a1c08b4.svg"
+  },
+  {
+    "revision": "00a79acb2b09ba2c729747032758357e",
+    "url": "/web/static/media/微软.00a79acb.svg"
+  },
+  {
+    "revision": "e08553ef412488c069ed8cbe9eec15ab",
+    "url": "/web/static/media/邮箱.e08553ef.svg"
+  },
+  {
+    "revision": "e7756ede123752c3fccbb9fa76b7615a",
+    "url": "/web/static/media/邮箱2.e7756ede.svg"
+  }
+]);
